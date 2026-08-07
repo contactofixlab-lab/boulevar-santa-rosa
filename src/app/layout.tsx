7998407017deb10df9223f1d98692492e258bbc6
Boulevard Santa Rosa <rcapcorp@gmail.com>
2026-08-06 22:21:48 -0400

    Use direct script tag for Mobysuite instead of Next.js Script component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import Script from "next/script";
 import { Nunito_Sans } from "next/font/google";
 import { WhatsAppFAB } from "@/components/layout/WhatsAppFAB";
 import { CotizadorDialogGlobal } from "@/components/sections/CotizadorDialogGlobal";
@@ -51,29 +50,25 @@ export default async function RootLayout({
         {/* Mobysuite Cotizador - Contenedor */}
         <div id="mobysuite-cotizador-boulevard"></div>
 
-        {/* Mobysuite Cotizador Script - Con atributos data-* correctos */}
-        <Script
-          id="mobysuite-script"
-          strategy="beforeInteractive"
+        {/* Mobysuite Cotizador Script - Direct script tag */}
+        <script
           dangerouslySetInnerHTML={{
             __html: `
-              (function() {
-                const script = document.createElement('script');
-                script.src = 'https://cdn.mobysuite.com/quote/js/app.js';
-                script.setAttribute('data-real-estate', 'inesdesuarez');
-                script.setAttribute('data-project-id', '4');
-                script.setAttribute('data-container', 'mobysuite-cotizador-boulevard');
-                script.setAttribute('data-template', 'default');
-                script.setAttribute('data-country-code', 'CL');
-                script.setAttribute('data-show-real-estate-logo', 'true');
-                script.setAttribute('data-show-project-logo', 'true');
-                script.setAttribute('data-primary-color', '#0671AE');
-                script.setAttribute('data-secondary-color', '#84CE25');
-                script.setAttribute('data-success-view-type', 'simple');
-                script.setAttribute('data-hide-selected-assets', 'false');
-                script.setAttribute('data-use-secondary-image', 'false');
-                document.body.appendChild(script);
-              })();
+              const script = document.createElement('script');
+              script.src = 'https://cdn.mobysuite.com/quote/js/app.js';
+              script.setAttribute('data-real-estate', 'inesdesuarez');
+              script.setAttribute('data-project-id', '4');
+              script.setAttribute('data-container', 'mobysuite-cotizador-boulevard');
+              script.setAttribute('data-template', 'default');
+              script.setAttribute('data-country-code', 'CL');
+              script.setAttribute('data-show-real-estate-logo', 'true');
+              script.setAttribute('data-show-project-logo', 'true');
+              script.setAttribute('data-primary-color', '#0671AE');
+              script.setAttribute('data-secondary-color', '#84CE25');
+              script.setAttribute('data-success-view-type', 'simple');
+              script.setAttribute('data-hide-selected-assets', 'false');
+              script.setAttribute('data-use-secondary-image', 'false');
+              document.body.appendChild(script);
             `,
           }}
         />
