928884d75397e3e6685fd8009d84c2fc9a4daa48
Boulevard Santa Rosa <rcapcorp@gmail.com>
2026-08-07 14:33:53 -0400

    Fix Mobysuite cotizador - use Script component instead of dangerouslySetInnerHTML

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,29 +46,6 @@ export default async function RootLayout({
         {children}
         <WhatsAppFAB />
         <CotizadorDialogGlobal tipologias={tipologias} />
-
-        {/* Mobysuite Cotizador Script - Direct script tag */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              const script = document.createElement('script');
-              script.src = 'https://cdn.mobysuite.com/quote/js/app.js';
-              script.setAttribute('data-real-estate', 'inesdesuarez');
-              script.setAttribute('data-project-id', '4');
-              script.setAttribute('data-container', 'mobysuite-cotizador-boulevard');
-              script.setAttribute('data-template', 'default');
-              script.setAttribute('data-country-code', 'CL');
-              script.setAttribute('data-show-real-estate-logo', 'true');
-              script.setAttribute('data-show-project-logo', 'true');
-              script.setAttribute('data-primary-color', '#0671AE');
-              script.setAttribute('data-secondary-color', '#84CE25');
-              script.setAttribute('data-success-view-type', 'simple');
-              script.setAttribute('data-hide-selected-assets', 'false');
-              script.setAttribute('data-use-secondary-image', 'false');
-              document.body.appendChild(script);
-            `,
-          }}
-        />
       </body>
     </html>
   );
